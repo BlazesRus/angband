26498fbbb98b0718192b06ea8e2f579bde7ee799
BlazesRus (James) <BlazesRus@yahoo.com>
2022-06-12 01:15:45 -0400

    Manual merge of angband.h

--- src/common/angband.h
+++ src/common/angband.h
@@ -1,6 +1,6 @@
 /**
- * \file angband.h
- * \brief Includes basic includes
+ * File: angband.h
+ * Purpose: Main Angband header file
  *
  * Copyright (c) 1997 Ben Harrison, James E. Wilson, Robert A. Koeneke
  *
@@ -13,31 +13,80 @@
 #define INCLUDED_ANGBAND_H
 
 /*
- * Include the low-level includes.
+ * Include the low-level includes
  */
 #include "h-basic.h"
 
+#ifndef SPClient
+#include "h-quark.h"
+#include "option.h"
+#endif
 /*
- * Include the mid-level includes.
+ * Include the mid-level includes
  */
 #include "z-bitflag.h"
 #include "z-color.h"
+
+#ifndef SPClient
+#include "z-file.h"
+#endif
+
 #include "z-form.h"
+
+#ifndef SPClient
+#include "z-msg.h"
+#include "z-rand.h"
+#include "z-set.h"
+#include "z-spells.h"
+#include "z-type.h"
+#endif
+
 #include "z-util.h"
 #include "z-virt.h"
-#include "z-rand.h"
+
+#ifndef SPClient
+#include "z-expression.h"
+#include "z-dice.h"
+#endif
 
 /*
- * Include the high-level includes.
+ * Include the high-level includes
  */
+#ifndef SPClient
+#include "z-defines.h"
+
+#include "buildid.h"
+#endif
+
 #include "config.h"
-#include "..\client\game-event.h"
+
+#ifndef SPClient
+#include "datafile.h"
+#include "guid.h"
+#include "md5.h"
+#include "source.h"
+#include "parser.h"
+#include "obj-common.h"
+#include "trap-common.h"
+#include "player-state.h"
+#include "mon-common.h"
+#include "player-common.h"
+#include "display.h"
+#include "obj-gear-common.h"
+#include "obj-tval.h"
+#include "player-common-calcs.h"
+#include "randname.h"
+#include "store-types.h"
+#include "util.h"
+
+#else
+
 #include "message.h"
 #include "player.h"
 
+#endif
 
 /***** Some older copyright messages follow below *****/
-
 
 /*
  * Note that these copyright messages apply to an ancient version
@@ -82,7 +131,13 @@
  *	 this software in any form without the expressed written consent
  *	 of the author Robert Alan Koeneke.
  */
-
-
+ 
+/* Horrible hack -- should we link lib math instead? */
+#ifndef min
+# define min(A, B) (A < B ? A : B)
+#endif
+#ifndef max
+# define max(A, B) (A > B ? A : B)
 #endif
 
+#endif
